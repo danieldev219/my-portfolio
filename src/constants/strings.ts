a1ede1144e54f37684e83cc9b5ce08244f39d7e4
danieldev219 <salceanudaniel43@gmail.com>
2025-03-14 08:05:47 -0700

    complete initial architecture

---
+++ src/constants/strings.ts
@@ -0,0 +1,26 @@
+class Strings {
+  static telegram = "Telegram";
+  static telegramLink = "https://telegram.me/superonedev";
+  static telegramUsername = "superonedev";
+
+  static github = "GitHub";
+  static githubLink = "https://www.github.com/danieldev219";
+  static githubUsername = "danieldev219";
+
+  static linkedIn = "LinkedIn";
+  static linkedInLink = "https://www.linkedin.com/in/salceanu-daniel-53a972355";
+  static linkedInUsername = "salceanu-daniel";
+
+  static discord = "Discord";
+  static discordLink = "https://www.discord.com/users/1197592643564212316";
+  static discordUsername = "techtrovedev";
+
+  static email = "Email";
+  static primaryEmailLink = "mailto:salceanudaniel43@gmail.com";
+  static primaryEmail = "salceanudaniel43@gmail.com";
+
+  static fullName = "Salceanu Daniel";
+  static shortName = "";
+}
+
+export default Strings;
---
+++ src/data/experiences.ts
@@ -0,0 +1,56 @@
+import { IExperienceItem } from "@/types";
+
+const experiences: IExperienceItem[] = [
+  {
+    designation: "Senior Fullstack Developer",
+    company: "DAG Voogule • Contract",
+    startDate: "Nov 2024",
+    endDate: "Mar 2025",
+    isCurrentJob: false,
+    location: "Remote - Dargenham, UK",
+    description: [
+      "Engineered secure and scalable full-stack solutions for a blockchain-based supply chain tracking application, focusing on web technology integration to reduce transaction costs by 15%.",
+      "Improved data transparency and platform accessibility by optimizing the frontend and back-end systems.",
+    ],
+  },
+  {
+    designation: "Full Stack AI Developer",
+    company: "Upwork • Freelance",
+    startDate: "Aug 2023",
+    endDate: "Mar 2025",
+    isCurrentJob: false,
+    location: "Dargenham, UK",
+    description: [
+      "Delivered 10+ web development projects with a focus on front-end frameworks (React.js, Vue.js) and back-end APIs (Express.js, Django) for global clients.",
+      "Developed AI agents and chatbot solutions that improved client workflow efficiency by up to 25%.",
+      "Maintained 95% client satisfaction rating, achieving repeat contracts and referrals.",
+    ],
+  },
+  {
+    designation: "Full Stack Software Developer",
+    company: "Entheology Project • Full time",
+    startDate: "Feb 2023",
+    endDate: "Jul 2023",
+    isCurrentJob: false,
+    location: "Remote - Lazuri, Romania",
+    description: [
+      "Built and optimized web-based AI-powered solutions, resulting in a 20% increase in platform user engagement.",
+      "Focused on web architecture and integration, improving user interaction with dynamic and responsive interfaces.",
+    ],
+  },
+  {
+    designation: "Full Stack Developer",
+    company: "Freelancer • Freelance",
+    startDate: "Feb 2023",
+    endDate: "Jul 2023",
+    isCurrentJob: false,
+    location: "Remote - Lazuri, Romania",
+    description: [
+      "Completed 30+ freelance software projects, achieving an average 4.8/5 client rating.",
+      "Led the development of 2 large-scale web platforms for AI integration, serving 5,000+ users.",
+      "Designed and implemented CI/CD pipelines to reduce deployment time by 50% and ensure continuous delivery."
+    ],
+  },
+];
+
+export default experiences;
